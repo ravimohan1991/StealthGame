755efd5f9a01a9b2df4efe3e30a0b6a713dfd662
Ravi Mohan <ravimohan1991@gmail.com>
2020-04-15 04:26:19 +0530

    Added extraction zone

---
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComp"));
+	BoxComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	BoxComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	BoxComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	BoxComp->SetBoxExtent(FVector(200.0f));
+	BoxComp->SetHiddenInGame(false);
+	RootComponent = BoxComp;
+}
+
+// Called when the game starts or when spawned
+void AFPSExtractionZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::OnOverlap);
+}
+
+// Called every frame
+void AFPSExtractionZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AFPSExtractionZone::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap with extraction zone"));
+}
+
